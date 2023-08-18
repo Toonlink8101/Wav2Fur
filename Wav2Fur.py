d45eb4f7928e9115f025a1c5bbd3ab3c71f09c4a
Toonlink8101 <toonlink8101@gmail.com>
2023-08-18 12:35:25 -0500

    Update Wav2Fur.py

--- Wav2Fur.py
+++ Wav2Fur.py
@@ -1,40 +1,16 @@
-import numpy as np
-import scipy
-from scipy.fft import rfft, rfftfreq, irfft
-# from matplotlib import pyplot as plt
+"""
+    Main function
+"""
 
-sample_rate, data = scipy.io.wavfile.read("what do you think it is.wav")
+data = []
 
-samples_per_frame = sample_rate / 60
-frames_in_data = len(data) / samples_per_frame
+data = Get_Data()
 
-frames = np.array_split(data, frames_in_data)
+data = Filter_Data(data)
 
-# recombine individual frames with its neighbor
-# blend these pairs with a hanning window
-window = np.hanning(samples_per_frame * 2)
+for segment in data:
+    pass
 
-yf = rfft(frames[6])
-xf = rfftfreq(len(frames[6]), 1/sample_rate)
+convert_data()
 
-chopped = irfft(yf)
-
-unframes = []
-
-for i in frames:
-    for j in i:
-        unframes.append(j)
-
-# print(unframes)
-
-# typed_unframes = np.empty(len(unframes))
-
-# for i in range(len(unframes)):
-#     typed_unframes[i] = unframes[i]
-#     print(typed_unframes)
-
-typed_unframes = np.copy(unframes)
-
-print(typed_unframes)
-
-# scipy.io.wavfile.write("unframesTest.wav", sample_rate, typed_unframes)
+output_data()
