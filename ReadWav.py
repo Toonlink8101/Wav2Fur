74e4d95c1c136513b7d953e5df19fc90039c23af
Toonlink8101 <toonlink8101@gmail.com>
2023-08-18 13:24:08 -0500

    Update ReadWav.py

--- ReadWav.py
+++ ReadWav.py
@@ -1,5 +1,13 @@
+import scipy
+import numpy as np
+
 """
-    Reads wav file and returns an array of 60hz audio intervals
+    Reads wav file and returns a list of 60hz audio intervals
 """
-def Get_Data():
-    pass
+def Get_Data(file_path: str) -> list[list]:
+    sample_rate, data = scipy.io.wavfile.read(file_path)
+
+    samples_per_frame = sample_rate / 60
+    frames_in_data = len(data) / samples_per_frame
+
+    return np.array_split(data, frames_in_data)
