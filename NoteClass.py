ec5a4009ae68562d315022784fea8127eca841ca
Toonlink8101 <toonlink8101@gmail.com>
2023-08-18 13:08:19 -0500

    Update NoteClass.py

--- NoteClass.py
+++ NoteClass.py
@@ -6,5 +6,5 @@ class Note:
         self.volume = volume
         self.detune = detune
 
-    def output_pattern(self) -> str:
+    def output_row(self) -> str:
         return str(Value2Note(self.value)) + ".." + hex(self.volume + 0x100)[-2:] + "E5" + hex(self.detune)
