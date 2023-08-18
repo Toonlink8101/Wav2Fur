082943f1e772486129ee075d03f0af74f30b2c40
Toonlink8101 <toonlink8101@gmail.com>
2023-08-18 15:30:37 -0500

    Update FilterHanning.py

--- FilterHanning.py
+++ FilterHanning.py
@@ -1,2 +1,12 @@
-def Filter_Data(data):
-    pass
+import numpy as np
+
+def Filter_Data(data:list[list]) -> list[list]:
+    # init
+    r = []
+    window = np.hanning(len())
+
+    # generate filtered data
+    for i in range(1, len(data)):
+        r.append(np.concatenate(data[i-1], data[i]))
+
+    
