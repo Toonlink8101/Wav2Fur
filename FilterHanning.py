7e88bedfd10c64929b84b715bb5f9ac62e4ef175
Toonlink8101 <toonlink8101@gmail.com>
2023-08-18 22:01:26 -0500

    Update FilterHanning.py

--- FilterHanning.py
+++ FilterHanning.py
@@ -3,7 +3,7 @@ import numpy as np
 def Filter_Data(data:list[list]) -> list[list]:
     # init
     r = []
-    window = np.hanning(len())
+    window = np.hanning(len(data[0]) * 2)
 
     # generate filtered data
     for i in range(1, len(data)):
