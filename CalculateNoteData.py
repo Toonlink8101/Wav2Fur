0827b785bfd6f9a0d4a26cf220029b5bf391b869
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 16:01:28 -0500

    Update CalculateNoteData.py

--- CalculateNoteData.py
+++ CalculateNoteData.py
@@ -5,6 +5,10 @@ from NoteClass import Note
 """
 
 def Get_Row(data:list, channel_count:int) -> list[Note]:
+    # escape condition
+    if channel_count <= 0:
+        return []
+    
     # find loudest frequency
 
     # isolate frequency
@@ -12,7 +16,10 @@ def Get_Row(data:list, channel_count:int) -> list[Note]:
     # determine volume
 
     # store results in an object
+    r = Note()
 
     # filter out frequency from data
-    
-    return 
+    filtered_data = data
+
+    # Call self
+    return [r] + Get_Row(filtered_data, channel_count-1)
