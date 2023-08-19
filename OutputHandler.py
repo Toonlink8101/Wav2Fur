3b04b53ac86a54897860b1ac04e1d8cbc056364e
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 18:14:55 -0500

    Update OutputHandler.py

--- OutputHandler.py
+++ OutputHandler.py
@@ -1,2 +1,21 @@
-def output_data():
+def output_pattern():
     pass
+
+def output_instrument():
+    pass
+
+def output_data():
+    
+    match input("""
+                \n1. Output to .fur module (not yet supported)
+                \n2. Output as pattern data
+                \n3. Output as instrument data
+                \nChoose a output format:
+            """):
+        case '2':
+            output_pattern()
+        case '3':
+            output_instrument()
+        case _:
+            print("Invalid option")
+            output_data()
