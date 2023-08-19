d6884d099c7565e653fa6d6c306f607fc3f2830b
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 16:07:38 -0500

    Update CalculateNoteData.py

--- CalculateNoteData.py
+++ CalculateNoteData.py
@@ -1,14 +1,19 @@
 from NoteClass import Note
+from scipy.fft import rfft, rfftfreq, irfft
 
 """
     Calulates frequencies for a tick of note data
 """
 
-def Get_Row(data:list, channel_count:int) -> list[Note]:
+def Get_Row(data:list, samplerate:int, channel_count:int) -> list[Note]:
     # escape condition
     if channel_count <= 0:
         return []
     
+    # fourier transform
+    yf = rfft(data)
+    xf = rfftfreq(len(data), 1/sample_rate)
+
     # find loudest frequency
 
     # isolate frequency
