bbf59467da2942c74eb734055c1fc887226c5745
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 15:45:21 -0500

    Create PromptUser.py

---
+++ PromptUser.py
@@ -0,0 +1,7 @@
+"""
+    Prompts user for file path and the number of channels
+    Returns the file path and channel count
+"""
+
+def Get_User_Data():
+    return input("Please input the file to be read: "), input("Input the number of channels: ")
