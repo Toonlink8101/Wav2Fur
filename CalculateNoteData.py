c3f0aac38eaa347586010e9ade8589759182dd38
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 15:52:35 -0500

    Update CalculateNoteData.py

--- CalculateNoteData.py
+++ CalculateNoteData.py
@@ -1,6 +1,18 @@
-def Get_Row():
+from NoteClass import Note
+
+"""
+    Calulates frequencies for a tick of note data
+"""
+
+def Get_Row(data:list, channel_count:int) -> list[Note]:
     # find loudest frequency
 
-    #
+    # isolate frequency
+
+    # determine volume
+
+    # store results in an object
+
+    # filter out frequency from data
     
-    pass
+    return 
