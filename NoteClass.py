12f131c8176f004564f997ad878adac8e361c13c
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 19:54:16 -0500

    Output maybe

--- NoteClass.py
+++ NoteClass.py
@@ -6,5 +6,5 @@ class Note:
         self.volume = volume
         self.detune = detune
 
-    def __str__(self) -> str:
+    def output(self) -> str:
         return str(Value2Note(self.value)) + ".." + hex(self.volume + 0x100)[-2:] + "E5" + hex(self.detune + 0x100)[-2:]
--- OutputHandler.py
+++ OutputHandler.py
@@ -1,23 +1,41 @@
 from NoteClass import Note
 
-def output_pattern():
+def output_pattern(data:list[list[Note]]):
+    output = "org.tildearrow.furnace - Pattern Data (166)\n0\n"
+    row_count = 256
+
+    # TODO: Implement clipboard
+    # TODO: implement row counter, user prompting
+    # TODO: prompt user for clipboard header
+
+    for row in data:
+        for note in row:
+            output += note.output() + '|'
+        output += "\n"
+
+    print(output)
+
+def output_instrument(data:list[list[Note]]):
     pass
 
-def output_instrument():
-    pass
-
-def output_note_data():
+def output_note_data(data:list[list[Note]]):
     
-    match input("""
-                \n1. Output to .fur module (not yet supported)
-                \n2. Output as pattern data
-                \n3. Output as instrument data
-                \nChoose a output format:
-            """):
-        case '2':
-            output_pattern()
-        case '3':
-            output_instrument()
-        case _:
-            print("Invalid option")
-            output_note_data()
+    while True:
+        match input("""
+                    \n1. Output to .fur module (not yet supported)
+                    \n2. Output as pattern data
+                    \n3. Output as instrument data
+                    \nChoose a output format:
+                """):
+            case '1':
+                print("Not yet supported")
+                continue
+            case '2':
+                output_pattern(data)
+                break
+            case '3':
+                output_instrument(data)
+                break
+            case _:
+                print("Invalid option")
+                continue
