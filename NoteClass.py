a791c846c18a93a7f60ef435eb408e617062ba5c
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 19:13:06 -0500

    set up freq2note

--- NoteClass.py
+++ NoteClass.py
@@ -6,5 +6,5 @@ class Note:
         self.volume = volume
         self.detune = detune
 
-    def output_row(self) -> str:
-        return str(Value2Note(self.value)) + ".." + hex(self.volume + 0x100)[-2:] + "E5" + hex(self.detune)
+    def __str__(self) -> str:
+        return str(Value2Note(self.value)) + ".." + hex(self.volume + 0x100)[-2:] + "E5" + hex(self.detune)[-2:]
---
+++ helpers/freq2note.py
@@ -0,0 +1,25 @@
+import math
+from NoteClass import Note
+
+def freq2note(frequency, loudness) -> int:
+    # reference frequency
+    f0 = 440
+
+    # number of semitones
+    n = 12 * math.log(frequency/f0, 2)
+
+    # calculate note value
+    semitones_from_f0 = int(n)
+
+    f0_value = 57 # value for A4
+    
+    note_value = f0_value + semitones_from_f0
+    
+    # calculate detune
+    semitone_fraction = n % 1
+    detune_value = round((0x80 * semitone_fraction) + 0x80)
+
+    # calculate volume
+    volume_value = 0 # fix this
+
+    return Note(note_value, volume_value, detune_value)
