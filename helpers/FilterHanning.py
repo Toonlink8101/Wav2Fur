ec55eba2aeffff20bb9905bb26e513e39a749241
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 20:17:44 -0500

    mono wav, trying to fix window issue

--- helpers/FilterHanning.py
+++ helpers/FilterHanning.py
@@ -3,7 +3,7 @@ import numpy as np
 def Filter_Data(data:list[list]) -> list[list]:
     # init
     r = []
-    window = np.hanning(len(data[0]) * 2)
+    window = np.hanning(len(data[0]))
 
     # generate filtered data
     for i in range(1, len(data)):
