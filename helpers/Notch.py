60cd1e8f219443def526719a41db5c654b5664dc
Toonlink8101 <toonlink8101@gmail.com>
2023-08-19 19:37:07 -0500

    Create Notch.py

---
+++ helpers/Notch.py
@@ -0,0 +1,15 @@
+"""
+    Code I stole again...
+"""
+
+from scipy import signal
+import numpy as np
+
+def notch_filter(data:list, notch_frequency:float, samplerate:int):
+    # Create/view notch filter
+    quality_factor = 20.0  # Quality factor
+    
+    # Design a notch filter using signal.iirnotch
+    b_notch, a_notch = signal.iirnotch(notch_frequency, quality_factor, samplerate)
+
+    return signal.filtfilt(b_notch, a_notch, data)
