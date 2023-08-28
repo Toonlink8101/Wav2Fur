e104a4b47aba7b2cbdb7182aebb5e56fb5a87520
Toonlink8101 <toonlink8101@gmail.com>
2023-08-27 22:13:39 -0500

    Trying to debug :/

--- GetRow.py
+++ GetRow.py
@@ -14,9 +14,7 @@ from itertools import islice
 def GetFrequencies(data: list, samplerate:int) -> dict:
     r = {}
 
-    series = note_freqs()
-
-    for current_frequency in islice(series, 1, 83):
+    for current_frequency in islice(note_freqs(), 1, 83):
         # if current_frequency%1000 <= 2:
         #     print(current_frequency//1000, "khz")
         filtered_frequency = butter_bandpass_filter(data, current_frequency-1, current_frequency+1, samplerate)
@@ -33,8 +31,9 @@ def Get_Row(data:list, samplerate:int, channel_count:int) -> list[Note]:
 
     frequencies = GetFrequencies(data, samplerate)
 
-    sorted_frequencies = sorted(frequencies.items(), key=lambda kv:(kv[1], kv[0]))
-    
+    #sorted_frequencies = sorted(frequencies.items(), key=lambda kv:(kv[1], kv[0]))
+    sorted_frequencies = sorted(frequencies.items(), key=lambda kv:(kv[1], kv[0]), reverse=True)
+
     # convert frequency and loudness to note objects
     for freq in sorted_frequencies[:channel_count]:
         row.append(freq2note(freq[0], freq[1]))
